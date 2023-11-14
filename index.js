01334120c9b8ed7e1cab6572de948ac17663f1e1
Bogdan Moldovan <34248960+bmoldo@users.noreply.github.com>
2023-11-14 20:26:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-   res.send('Hello World from VSP!');
+   res.send('Hello World from Docker!');
 });
 
 app.listen(PORT, () => {
